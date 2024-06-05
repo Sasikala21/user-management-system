b29fafd8714415bf081e05ab11fa981480e8c2d8
SasikalaSasi21 <sasikala.s@thinkinfinity.co.in>
2024-06-05 12:08:23 +0530

    Revert "user management code impl"
    This reverts commit 6cd5f0a782af6eaf95813b0bd2a5fbe0e1092261.

--- src/middlewares/bcrypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const securePassword = async (password) => {
-    const salt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, 	salt);
-    return hashedPassword;
-};
-module.exports = securePassword;
